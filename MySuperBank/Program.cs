e2d52a1b37d662233d1e32dd1accb1e984a2f0e4
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2021-09-26 19:37:04 -0300

    Add project files.

---
+++ MySuperBank/Program.cs
@@ -0,0 +1,19 @@
+﻿using BankyStuffLibrary;
+using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Eduardo", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            account.MakeWithdrawal(50, DateTime.Now, "Xbox Game");
+
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+}
